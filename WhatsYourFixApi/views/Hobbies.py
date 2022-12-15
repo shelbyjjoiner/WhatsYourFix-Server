1f9cd9652941edf4aee6d045999379882c1d58bb
shelbyjjoiner <shelbyjjoiner@yahoo.com>
2022-12-15 10:21:21 -0600

    changed minor method request

--- WhatsYourFixApi/views/Hobbies.py
+++ WhatsYourFixApi/views/Hobbies.py
@@ -2,15 +2,10 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from WhatsYourFixApi.models import Hobbies 
+from WhatsYourFixApi.models import Hobbies
 
 class HobbieView(ViewSet):
 
-    def retrieve(self, request, pk=None):
-        hobby = Hobbies.objects.get()
-
-        serializer = HobbieSerializer(hobby,  context={'request':request})
-        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
 
     def list(self, request):
 
@@ -18,22 +13,33 @@ class HobbieView(ViewSet):
         serializer = HobbieSerializer(hobbies, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def create(self, request): 
+
+    def retrieve(self, request, pk):
+        hobby = Hobbies.objects.get(pk=pk)
+
+        serializer = HobbieSerializer(hobby, context={'request':request})
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+
+
+    def create(self, request):
 
         new_hobbie = Hobbies.objects.create(
             label = request.data["label"]
         )
         new_hobbie.save()
-        
+
         serializer = HobbieSerializer(new_hobbie, many=False)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    def destroy(self, pk):
-        hobby = Hobbies.objects.get(pk=pk)
-        hobby.delete()
-        return Response(None, status=status.HTTP_204_NO_CONTENT)
+    def update(self, request, pk):
+        new_hobbie = Hobbies.objects.get(pk=pk)
+        new_hobbie.label= request.data["label"]
+        new_hobbie.save()
+
+        return Response (None, status=status.HTTP_204_NO_CONTENT)
+
 
 class HobbieSerializer(serializers.ModelSerializer):
-    class Meta: 
+    class Meta:
         model = Hobbies
         fields = ('id', 'label',)
